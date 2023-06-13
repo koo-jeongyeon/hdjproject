221f065a49041e4b52b4c23818486de62b86f08a
KooJeongYeon <noey458749@gmail.com>
2023-06-13 14:36:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/hdjproject/HdjprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hdjproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdjprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdjprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hdjproject/HdjprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hdjproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdjprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
